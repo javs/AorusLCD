391abc81966b1cdb6791ecdf128818a0a04d877b
Javier Joaquin <javierous@gmail.com>
2020-12-06 23:24:37 -0500

    Initial version with on/off support

--- galcd/AorusLCDController.h
+++ galcd/AorusLCDController.h
@@ -1,5 +1,33 @@
 #pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <cstdint>
+
+
 class AorusLCDController
 {
+	HMODULE m_gvdisplay = nullptr;
+
+
+	using TGvReadI2C = uint32_t(*)(uint32_t, void*, void*, uint32_t);
+	TGvReadI2C _GvReadI2C = nullptr;
+
+	using TGvWriteI2C = uint32_t(*)(uint32_t, void*);
+	TGvWriteI2C _GvWriteI2C = nullptr;
+
+	using TGvInitDispLib = uint32_t(*)();
+	TGvInitDispLib _GvInitDispLib = nullptr;
+
+	using TGvFreeDispLib = uint32_t(*)();
+	TGvFreeDispLib _GvFreeDispLib = nullptr;
+
+public:
+	AorusLCDController();
+	~AorusLCDController();
+
+	uint32_t SetLCDStatus(bool on_off);
+	void ReadI2C();
 };
 
--- galcd/galcd.cpp
+++ galcd/galcd.cpp
@@ -1,8 +1,41 @@
-#include <iostream>
 
-int main()
+#include <iostream>
+#include <string>
+
+#include "AorusLCDController.h"
+
+void usage()
 {
-    std::cout << "Hello World!\n";
+    std::cerr << "Usage: galcd [on|off]\n";
+}
+
+int main(int argc, char* argv[])
+{
+    AorusLCDController lcd{};
+    
+    if (argc != 2)
+    {
+        usage();
+        return 1;
+    }
+
+    bool desired;
+    if (std::string{ "on" } == argv[1])
+        desired = true;
+    else if (std::string{ "off" } == argv[1])
+        desired = false;
+    else {
+        usage();
+        return 2;
+    }
+
+    uint32_t result = lcd.SetLCDStatus(desired);
+    
+    if (result != 0)
+    {
+        std::cout << "Failed. Result:" << std::hex << result << "\n";
+        return 3;
+    }
 
     return 0;
 }
